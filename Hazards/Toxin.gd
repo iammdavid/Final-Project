dea69e44605c1a6cce75d8e3b467590db0126870
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2021-12-02 19:53:22 -0500

    oops let's just remove that feature

--- Hazards/Toxin.gd
+++ Hazards/Toxin.gd
@@ -1,10 +1,10 @@
-extends KinematicBody2D
+extends Node2D
 
-onready var player = get_node_or_null("root/Game/Player")
+onready var player = get_node("root/Game/Player")
 
 func _on_Area2D_area_entered(player):
-	player.set_timer_damage()
+	player.set_timer_damage_on()
 
 
 func _on_Area2D_area_exited(player):
-	player.set_timer_damage()
+	player.set_timer_damage_off()
